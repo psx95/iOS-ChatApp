40bb70889d7ad37a4fc82f2312fc0147a4771209
psx95 <pranav.ps95@hotmail.com>
2019-02-07 23:42:54 +0530

    feat(App): Implements the user registration process through firebase

--- Flash Chat/AppDelegate.swift
+++ Flash Chat/AppDelegate.swift
@@ -20,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         //TODO: Initialise and Configure your Firebase here:
         FirebaseApp.configure()
+        let myDatabase = Database.database().reference()
+        myDatabase.setValue("My data")
         return true
     }
 
--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 
 class RegisterViewController: UIViewController {
@@ -29,15 +30,16 @@ class RegisterViewController: UIViewController {
   
     @IBAction func registerPressed(_ sender: AnyObject) {
         
-
-        
         //TODO: Set up a new user on our Firbase database
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text! ) { (user, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("Registration Successful !")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
         
-        
-
-        
-        
-    } 
-    
+    }
     
 }
